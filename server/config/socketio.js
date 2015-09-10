9aae8e98d9b01b5f0d130e10be5c10675f81ebd7
Jesus Villarroel <jvr42@hotmail.com>
2015-09-10 01:58:28 -0300

    1st message

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -25,21 +25,6 @@ function onConnect(socket) {
 }
 
 module.exports = function(socketio) {
-  // socket.io (v1.x.x) is powered by debug.
-  // In order to see all the debug output, set DEBUG (in server/config/local.env.js) to including the desired scope.
-  //
-  // ex: DEBUG: "http*,socket.io:socket"
-
-  // We can authenticate socket.io users and access their token through socket.decoded_token
-  //
-  // 1. You will need to send the token in `client/components/socket/socket.service.js`
-  //
-  // 2. Require authentication here:
-  // socketio.use(require('socketio-jwt').authorize({
-  //   secret: config.secrets.session,
-  //   handshake: true
-  // }));
-
   socketio.on('connection', function(socket) {
     socket.address = socket.request.connection.remoteAddress +
       ':' + socket.request.connection.remotePort;
@@ -48,8 +33,8 @@ module.exports = function(socketio) {
 
 /*    socket.log = function(...data) {
       console.log(`SocketIO ${socket.nsp.name} [${socket.address}]`, ...data);
-    };*/
-
+    };
+*/
     // Call onDisconnect.
     socket.on('disconnect', function() {
       onDisconnect(socket);
@@ -61,4 +46,3 @@ module.exports = function(socketio) {
     socket.log('CONNECTED');
   });
 });
-}
