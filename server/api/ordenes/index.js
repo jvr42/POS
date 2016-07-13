c4b5d3ee3c13e143d2e5aca702901d86c105abad
Jesus Villarroel <jvr42@hotmail.com>
2016-07-13 01:22:16 -0400

    updating reportes

--- server/api/ordenes/index.js
+++ server/api/ordenes/index.js
@@ -6,10 +6,11 @@ var controller = require('./ordenes.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
+router.get('/user/:user', controller.user);
+router.get('/:desde/:hasta', controller.limited);
 router.get('/all', controller.indexAll);
 router.get('/insights/index', controller.insights);
 router.get('/:id', controller.show);
-router.get('/user/:user', controller.user);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
