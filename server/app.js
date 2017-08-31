14e6439021139e68e58658707c3614d3da9c7e94
Jesus Villarroel <jvr42@hotmail.com>
2017-08-30 22:03:30 -0300

    starting again :/

--- server/app.js
+++ server/app.js
@@ -12,14 +12,14 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to MongoDB
-mongoose.connect(config.mongo.uri, config.mongo.options);
+/*mongoose.connect(config.mongo.uri, config.mongo.options);
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
 });
 
 // Populate databases with sample data
-if (config.seedDB) { require('./config/seed'); }
+if (config.seedDB) { require('./config/seed'); }*/
 
 // Setup server
 var app = express();
