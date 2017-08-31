77385e93e0f82cea9b8ed7941a49f85218a29568
Jesus Villarroel <jvr42@hotmail.com>
2017-08-31 00:48:04 -0300

    found enviromental variable

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -21,6 +21,6 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL +
             process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://172.30.11.197/pos'
+            'mongodb://userAYV:QjUYvkOjemeNuCH3@172.30.11.197/pos'
   }
 };
