41c165a522b0e5eef1bb44bd12c64ef7be7580da
Jesus Villarroel <jvr42@hotmail.com>
2017-09-05 18:29:19 -0300

    planning

--- server/app.js
+++ server/app.js
@@ -12,12 +12,12 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to MongoDB
-mongoose.Promise = global.Promise;
+/*mongoose.Promise = global.Promise;
 mongoose.connect(config.mongo.uri, config.mongo.options);
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
-});
+});*/
 
 // Populate databases with sample data
 if (config.seedDB) { require('./config/seed'); }
