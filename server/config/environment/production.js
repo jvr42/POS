4e56e24dc6de5e00206b9509de697b6cac0b3424
Jesus Villarroel <jvr42@hotmail.com>
2017-09-04 22:51:17 -0300

    troubleshooting socket functionality ip based 2

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
-            '172.30.222.30',
+            '127.0.0.1',
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
