78f5ee9e698a71d99995857ac1bf6eec08e06c98
Jesus Villarroel <jvr42@hotmail.com>
2017-09-05 17:41:23 -0300

    Merging both services into one

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL +
             process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@127.0.0.1/' + process.env.MONGODB_DATABASE
+            'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@localhost/' + process.env.MONGODB_DATABASE
   }
 };
