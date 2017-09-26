daa1ee4ed965ceb2d8b0e3748be8211e0f059481
jvr42 <jvr42@hotmail.com>
2017-09-26 16:11:24 -0300

    Update production.js

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL +
             process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://userTHK:mEfWJkSNjyjRyn6J@mongodb/pos'
+            'mongodb://user0Q1:Uj78YseNnPSUinGI@mongodb/pos'
   }
 };
