75e1a7d701683660451829bbe53b680bea91a6aa
jvr42 <jvr42@hotmail.com>
2017-09-27 11:22:31 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,14 +11,14 @@ var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
 
-/*
+
 // Connect to MongoDB
 mongoose.connect(config.mongo.uri, config.mongo.options);
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
 });
-*/
+
 
 // Populate databases with sample data
 if (config.seedDB) { require('./config/seed'); }
